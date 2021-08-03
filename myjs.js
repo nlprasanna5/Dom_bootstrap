16f8e88fcbae5917786b0aaeaebe6a9fcc12e487
Narapureddy Lakshmi Prasanna <lakshmi.76740@gmail.com>
2021-08-03 15:11:27 +0530

    added delete functionality in js

--- myjs.js
+++ myjs.js
@@ -1,14 +1,14 @@
 const taskContainer = document.querySelector(".task__container");
-const globalStore = [];
+let globalStore = [];
 console.log(taskContainer);
 
 const generateNewCard = (taskData) => `
-  <div class="col-sm-12 col-md-6 col-lg-4" id=${taskData.id}>
+  <div class="col-sm-12 col-md-6 col-lg-4" >
                   <div class="card">
                       <!--card header-->
                       <div class="card-header d-flex justify-content-end gap-2">
                         <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
-                        <button type="button" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
+                        <button type="button" class="btn btn-outline-danger" id=${taskData.id} onclick="deleteCard.apply(this,arguments)"><i class="fas fa-trash-alt" id=${taskData.id} onclick="deleteCard.apply(this,arguments)"></i></button>
                       </div>
                       <!--card body-->
                       <div class="card-body">
@@ -40,7 +40,23 @@ const loadInitialCardData = () => {
   )
 };
 
+//Delete function
 
+const deleteCard = (event) => {
+  event = window.event;
+  const targetID = event.target.id;
+  const tagname = event.target.tagName;
+
+  globalStore = globalStore.filter((cardObject) => cardObject.id !== targetID);
+  localStorage.setItem("tasky",JSON.stringify({cards: globalStore}));
+
+  if(tagname === "BUTTON") {
+    return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode);
+  }
+  else {
+    return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
+  }
+};
 
 
 
